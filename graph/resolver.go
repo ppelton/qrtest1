bdcd45de1a128a941efc45c7cc951a97dbbf31ab
Paul Pelton <ppelton@gmail.com>
2025-02-19 14:51:40 -0500

    graphql implementation with gqlgen

---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+import (
+	"qrtest1.peltonium.net/graph/model"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	items []*model.Item
+}
